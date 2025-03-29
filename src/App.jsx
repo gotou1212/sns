46c5309c120b6f3e93ecf96d0b05cff8a5c23c5f
gotou1212 <meijiyou.20081212@gmail.com>
2025-03-29 03:00:42 +0000

    a

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar";
+import Timeline from "./components/Timeline";
+const app = () => {
+  return(
+    <div>
+      <Sidebar />
+      <Timeline />
+      {/* <suggestion /> */}
+    </div>
+  );
+};
+
+export default app;
