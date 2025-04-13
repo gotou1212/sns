906a663ad640cd702f701d508c979ce703505813
gotou1212 <meijiyou.20081212@gmail.com>
2025-04-13 03:09:59 +0000

    a

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -2,7 +2,14 @@ import './Suggestion.css'
 
 export const Suggestion = () => {
     return(
-        <div className="suggestion">suggestion</div>
+        <div className="suggestion">
+               <input type="text"  className="kennsaku" placeholder="検索"/>   
+               <div className="kounyuu">
+                <h2 className="hutomozi">プレミアムサブスクにアップグレード</h2>
+                <h3 classname="kae">買え</h3>
+                <button className="kounyuubotann">購入する</button>
+               </div>  
+        </div>
     )
 }
 
