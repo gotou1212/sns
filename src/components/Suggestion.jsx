75a85a9e5e4baa3a0e8cdebb9713076fc6102ff6
gotou1212 <meijiyou.20081212@gmail.com>
2025-04-17 08:58:59 +0000

    a

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -3,12 +3,19 @@ import './Suggestion.css'
 export const Suggestion = () => {
     return(
         <div className="suggestion">
-               <input type="text"  className="kennsaku" placeholder="検索"/>   
-               <div className="kounyuu">
-                <h2 className="hutomozi">プレミアムサブスクにアップグレード</h2>
+            <input type="text"  className="kennsaku" placeholder="検索"/>   
+            <div className="kounyuu">
+                <h1 className="hutomozi">プレミアムサブスクにアップグレード</h1>
                 <h3 classname="kae">買え</h3>
-                <button className="kounyuubotann">購入する</button>
-               </div>  
+                <button className="kounyuubotann" 
+                    onClick={() => window.open("https://www.google.com/webhp?hl=ja&ictx=0&sa=X&ved=0ahUKEwjtvf-Kht6MAxWGh1YBHXZ2DUcQpYkNCAs","_blank")}>
+                    購入する
+                </button>
+            </div>  
+            <div className="a">
+                <h1>今何してる？</h1>
+
+            </div>
         </div>
     )
 }
