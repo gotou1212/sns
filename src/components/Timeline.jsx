8814985260c89d669e60d95601b80bda81704e01
gotou1212 <meijiyou.20081212@gmail.com>
2025-05-25 02:51:50 +0000

    A

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -10,22 +10,51 @@ export const Timeline = () => {
             <button className="button">投稿</button>
             </div>
           </div>
-          <div className="kakoi3">
-            <h3>uaa</h3>
-            <div className="site">
-            <div className='buttons'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
+          <div className="post-card">
+        <div className="post-icon-area">
+          <div className="post-icon"></div>
+        </div>
+
+        <div className="post-content-area">
+          <div className="post-content-header">
+            <div className="post-content-header-left">
+              <div className="post-content-header-username">Username</div>
+              <div className="post-content-header-id">@ID</div>
+              <div className="post-content-header-time">14時間</div>
             </div>
-            <div className="button56">
-            <button>5</button>
-            <button>6</button>
-            </div>
-            </div>
+            <div className="post-content-header-right">
+              <div className="">・・・</div>
             </div>
           </div>
+          <div className="post-content-main">main</div>
+          <div className="post-content-image">image</div>
+          <div className="post-content-buttons">
+           <div className="left">
+            <div className="button1">
+            <button>1</button>
+            </div>
+            <div className="button2">
+            <button>2</button>
+            </div>
+            <div className="button3">
+            <button>3</button>
+            </div>
+            <div className="4">
+            <button>4</button>
+            </div>
+           </div>
+           <div className="right">
+            <div classNAme="button5">
+            <button>5</button>
+            </div>
+            <div className="6">
+            <button>6</button>
+            </div>
+           </div>
+          </div>
+        </div>
+      </div>  
+      </div>
 
       
     );
