48261a8be1f2dceca52e10494a3b29f3136ff445
gotou1212 <meijiyou.20081212@gmail.com>
2025-06-07 13:43:21 +0000

    A

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import './Sidebar.css'
 import { HomeButton } from './HomeButton';
-import {SearchButton} from './SearchButton';
-import {NotificationButton} from './NotificationButton';
+import { SearchButton } from './SearchButton';
+import { NotificationButton } from './NotificationButton';
 import { MailButton } from './MailButton';
 import { GrokButton } from './GrokButton';
 import { BookmarkButton } from './BookmarkButton';
-import { RecruitmentButton } from './Recruitment';
+import { RecruitmentButton } from './RecruitmentButton';
 import { CommunityButton } from './CommunityButton';
 import { PremiumButton } from './PremiumButton';
 import { VerifiedorganizationButton } from './VerifiedorganizationButton';
