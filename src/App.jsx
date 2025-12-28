81288a9120298a71bf86b6add77095e691ce8155
gotou1212 <meijiyou.20081212@gmail.com>
2025-12-28 11:50:34 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,15 @@ const App = () => {
       <Sidebar />
       <Timeline />
       <Suggestion />
+    
+    <div className ='sider-post-button'> 
+    ポストする
+    </div>
+
+    <a href="/login">ログイン</a>
+    <a href="/signup">登録</a>
     </div>
   );
-};
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import LoginPage  from "./pages/Loginpage";
+import SignupPage  from "./pages/SignupPage";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <div>Login page</div>
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />
   }
 ]);
 createRoot(document.getElementById('root')).render(
